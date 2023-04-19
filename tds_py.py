5127c77767eab72bdc2cf8b23aa170604861161e
shilparoypain <131286815+shilparoypain@users.noreply.github.com>
2023-04-19 22:49:06 +0530

    Update tds_py.py

--- tds_py.py
+++ tds_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1gOq8KP-8ebqakUfph2UkivJW28mZvHB1
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
